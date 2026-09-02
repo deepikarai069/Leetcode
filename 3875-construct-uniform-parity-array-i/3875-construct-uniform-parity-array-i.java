16775b84432a03eb2a434b27da5efbafc2dd90fc
Deepika Rai <raideepika069@gmail.com>
2026-09-02 09:55:51 +0530

    Time: 1 ms (31.89%), Space: 45.2 MB (57.87%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // Any single element can always be chosen as it is.
+        // If there are both even and odd numbers, we can make
+        // every element have the same parity using subtraction.
+
+        return true;
+    }
+}
