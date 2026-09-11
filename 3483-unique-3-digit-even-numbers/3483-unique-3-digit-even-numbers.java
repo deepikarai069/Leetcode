08e859ad6d923c216925bbae80dc6c3134f6ce9c
Deepika Rai <raideepika069@gmail.com>
2026-09-11 19:30:50 +0530

    Time: 1 ms (100%), Space: 43.8 MB (93.07%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        int[] count = new int[10];
+
+        for (int d : digits) {
+            count[d]++;
+        }
+
+        int ans = 0;
+
+        // Hundreds digit: 1-9
+        for (int a = 1; a <= 9; a++) {
+            if (count[a] == 0) continue;
+
+            count[a]--;
+
+            // Tens digit: 0-9
+            for (int b = 0; b <= 9; b++) {
+                if (count[b] == 0) continue;
+
+                count[b]--;
+
+                // Units digit must be even
+                for (int c = 0; c <= 8; c += 2) {
+                    if (count[c] > 0) {
+                        ans++;
+                    }
+                }
+
+                count[b]++;
+            }
+
+            count[a]++;
+        }
+
+        return ans;
+    }
+}
